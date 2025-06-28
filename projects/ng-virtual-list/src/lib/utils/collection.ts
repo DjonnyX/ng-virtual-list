8dc8699f19fae409a886509f37a81383bf847473
Evgenii Grebennikov <djonnyx@gmail.com>
2025-06-28 09:23:37 +0300

    hotfix: #64 implemented clearing of cache of deleted collection elements

---
+++ projects/ng-virtual-list/src/lib/utils/collection.ts
@@ -0,0 +1,30 @@
+import { Id } from "../types"
+
+/**
+ * Returns the removed or updated elements of a collection.
+ */
+export const getCollectionRemovedOrUpdatedItems = <I extends { id: Id }, C extends Array<I>>(previousCollection: C | null | undefined, currentCollection: C | null | undefined): C => {
+    const result = new Array<I>() as C;
+    if (!currentCollection || currentCollection.length === 0 || !previousCollection || previousCollection.length === 0) {
+        return (previousCollection ? [...previousCollection] : []) as C;
+    }
+    const collectionDict: { [id: Id]: I } = {};
+    for (let i = 0, l = currentCollection.length; i < l; i++) {
+        const item = currentCollection[i];
+        if (item) {
+            collectionDict[item.id] = item;
+        }
+    }
+    for (let i = 0, l = previousCollection.length; i < l; i++) {
+        const item = previousCollection[i], id = item.id;
+        if (item) {
+            if (collectionDict.hasOwnProperty(id)) {
+                if (item === collectionDict[id]) {
+                    continue;
+                }
+            }
+            result.push(item);
+        }
+    }
+    return result;
+}
