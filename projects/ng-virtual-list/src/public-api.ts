669a57ef5a39424afb17729589ae53bb0cb244b2
Evgenii Grebennikov <djonnyx@gmail.com>
2025-07-19 09:15:14 +0300

    hotfix: #217 library assembly fixed

--- projects/ng-virtual-list/src/public-api.ts
+++ projects/ng-virtual-list/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ng-virtual-list
  */
 
+export * from './lib/ng-virtual-list.module';
 export * from './lib/ng-virtual-list.component';
 export * from './lib/enums';
 export * from './lib/models';
