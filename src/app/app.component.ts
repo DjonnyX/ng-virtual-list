d24aabed8214b42eb4e8a4065c529beda1f92795
Evgenii Grebennikov <djonnyx@gmail.com>
2025-07-25 15:33:12 +0300

    feat: #269 implemented end position in stickyMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { IVirtualListCollection, IVirtualListStickyMap, IVirtualListItem } from 
 import { Id } from '../../projects/ng-virtual-list/src/lib/types';
 import { LOGO } from './const';
 
-const MAX_ITEMS = 10000;
+const MAX_ITEMS = 50000;
 
 const ITEMS: IVirtualListCollection = [];
 for (let i = 0, l = MAX_ITEMS; i < l; i++) {
@@ -30,7 +30,8 @@ const HORIZONTAL_GROUP_ITEMS: IVirtualListCollection = [],
 for (let i = 0, l = MAX_ITEMS; i < l; i++) {
   const id = i + 1, type = i === 0 || Math.random() > .895 ? 'group-header' : 'item';
   HORIZONTAL_GROUP_ITEMS.push({ id, type, name: type === 'group-header' ? getGroupName() : `${id}` });
-  HORIZONTAL_GROUP_ITEMS_STICKY_MAP[id] = type === 'group-header' ? 1 : 0;
+  const pos = 1 + Math.round(Math.random()), actualPos = pos === 1 ? 1 : 2;
+  HORIZONTAL_GROUP_ITEMS_STICKY_MAP[id] = type === 'group-header' ? actualPos : 0;
 }
 
 const GROUP_ITEMS: IVirtualListCollection = [],
@@ -43,7 +44,8 @@ for (let i = 0, l = MAX_ITEMS; i < l; i++) {
     groupIndex++;
   }
   GROUP_ITEMS.push({ id, type, name: type === 'group-header' ? `Group ${groupIndex}` : `Item: ${id}` });
-  GROUP_ITEMS_STICKY_MAP[id] = type === 'group-header' ? 1 : 0;
+  const pos = 1 + Math.round(Math.random()), actualPos = pos === 1 ? 1 : 2;
+  GROUP_ITEMS_STICKY_MAP[id] = type === 'group-header' ? actualPos : 0;
 }
 
 const CHARS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -80,15 +82,16 @@ const GROUP_DYNAMIC_ITEMS: IVirtualListCollection = [],
   GROUP_DYNAMIC_ITEMS_STICKY_MAP_WITH_SNAP: IVirtualListStickyMap = {};
 
 let groupDynamicIndex = 0;
-for (let i = 0, l = 100000; i < l; i++) {
+for (let i = 0, l = MAX_ITEMS; i < l; i++) {
   const id = i + 1, type = i === 0 || Math.random() > .895 ? 'group-header' : 'item';
   if (type === 'group-header') {
     groupDynamicIndex++;
   }
   GROUP_DYNAMIC_ITEMS.push({ id, type, name: type === 'group-header' ? `Group ${id}. ${generateText()}` : `${id}. ${generateText()}` });
-  GROUP_DYNAMIC_ITEMS_STICKY_MAP[id] = type === 'group-header' ? 1 : 0;
+  const pos = 1 + Math.round(Math.random()), actualPos = pos === 1 ? 1 : 2;
+  GROUP_DYNAMIC_ITEMS_STICKY_MAP[id] = type === 'group-header' ? actualPos : 0;
   GROUP_DYNAMIC_ITEMS_WITH_SNAP.push({ id, type, name: type === 'group-header' ? `Group ${id}` : `${id}. ${generateText()}` });
-  GROUP_DYNAMIC_ITEMS_STICKY_MAP_WITH_SNAP[id] = type === 'group-header' ? 1 : 0;
+  GROUP_DYNAMIC_ITEMS_STICKY_MAP_WITH_SNAP[id] = type === 'group-header' ? actualPos : 0;
 }
 
 @Component({
@@ -98,11 +101,11 @@ for (let i = 0, l = 100000; i < l; i++) {
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  readonly logo = LOGO;
+
   protected _listContainerRef = viewChild('virtualList', { read: NgVirtualListComponent });
 
   protected _dynamicListContainerRef = viewChild('dynamicList', { read: NgVirtualListComponent });
-
-  readonly logo = LOGO;
 
   items = ITEMS;
 
