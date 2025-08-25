089173d3d283d520550b37fdd4d89ee14a42128a
Evgenii Grebennikov <djonnyx@gmail.com>
2025-08-25 08:01:55 +0300

    feat: 19.x #352 added odd & even indicators to list items

--- projects/ng-virtual-list/src/lib/components/ng-virtual-list-item.component.ts
+++ projects/ng-virtual-list/src/lib/components/ng-virtual-list-item.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Temp
 import { IRenderVirtualListItem } from '../models/render-item.model';
 import { ISize } from '../types';
 import {
-  DEFAULT_ZINDEX, DISPLAY_BLOCK, DISPLAY_NONE, HIDDEN_ZINDEX, POSITION_ABSOLUTE, POSITION_STICKY, PX, SIZE_100_PERSENT,
+  DEFAULT_ZINDEX, DISPLAY_BLOCK, DISPLAY_NONE, HIDDEN_ZINDEX, PART_DEFAULT_ITEM, PART_ITEM_EVEN, PART_ITEM_ODD, PART_ITEM_SNAPPED, POSITION_ABSOLUTE, POSITION_STICKY, PX, SIZE_100_PERSENT,
   SIZE_AUTO, TRANSLATE_3D, VISIBILITY_HIDDEN, VISIBILITY_VISIBLE, ZEROS_TRANSLATE_3D,
 } from '../const';
 import { BaseVirtualListItemComponent } from '../models/base-virtual-list-item-component';
@@ -30,6 +30,9 @@ export class NgVirtualListItemComponent extends BaseVirtualListItemComponent {
     return this._id;
   }
 
+  private _part = PART_DEFAULT_ITEM;
+  get part() { return this._part; }
+
   regular: boolean = false;
 
   data: IRenderVirtualListItem | undefined;
@@ -40,6 +43,8 @@ export class NgVirtualListItemComponent extends BaseVirtualListItemComponent {
     }
 
     this.data = v;
+
+    this.updatePartStr(v);
 
     this.update();
 
@@ -112,6 +117,20 @@ export class NgVirtualListItemComponent extends BaseVirtualListItemComponent {
     }
 
     this._cdr.markForCheck();
+  }
+
+  private updatePartStr(v: IRenderVirtualListItem | undefined) {
+    let odd = false;
+    if (v?.index !== undefined) {
+      odd = v.index % 2 === 0;
+    }
+
+    let part = PART_DEFAULT_ITEM;
+    part += odd ? PART_ITEM_ODD : PART_ITEM_EVEN;
+    if (v ? v.config.snapped : false) {
+      part += PART_ITEM_SNAPPED;
+    }
+    this._part = part;
   }
 
   getBounds(): ISize {
