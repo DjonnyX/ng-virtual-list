9a5da2128f26948fac0a1cffba5fb5eeca09de7a
Evgenii Grebennikov <djonnyx@gmail.com>
2025-09-09 17:58:57 +0300

    feat: 14.x #381 Implemented select, multiselect and unselectable list modes

---
+++ projects/ng-virtual-list/src/lib/utils/isMethodForSelecting.ts
@@ -0,0 +1,20 @@
+import { MethodForSelecting, MethodsForSelecting } from "../enums";
+
+const NONE_ALIASES = [MethodsForSelecting.NONE, 'none'],
+    SELECT_ALIASES = [MethodsForSelecting.SELECT, 'select'],
+    MULTI_SELECT_ALIASES = [MethodsForSelecting.MULTI_SELECT, 'multi-select'];
+
+/**
+ * Defines the method for selecting list items.
+ * @link https://github.com/DjonnyX/ng-virtual-list/blob/14.x/projects/ng-virtual-list/src/lib/utils/isMethodForSelecting.ts
+ * @author Evgenii Grebennikov
+ * @email djonnyx@gmail.com
+ */
+export const isMethodForSelecting = (src: MethodForSelecting, expected: MethodForSelecting): boolean => {
+    if (NONE_ALIASES.includes(expected)) {
+        return NONE_ALIASES.includes(src);
+    } else if (SELECT_ALIASES.includes(expected)) {
+        return SELECT_ALIASES.includes(src);
+    }
+    return MULTI_SELECT_ALIASES.includes(src);
+}
