daa441fef01c98ea5be6ac24c11180f19cf70d86
Evgenii Grebennikov <djonnyx@gmail.com>
2025-09-24 11:22:20 +0300

    feat: #496 added collection mode property

---
+++ projects/ng-virtual-list/src/lib/utils/isCollectionMode.ts
@@ -0,0 +1,17 @@
+import { CollectionMode, CollectionModes } from "../enums";
+
+const NORMAL_ALIASES = [CollectionModes.NORMAL, 'normal'],
+    LAZY_ALIASES = [CollectionModes.LAZY, 'lazy'];
+
+/**
+ * Determines the axis membership of a virtual list
+ * @link https://github.com/DjonnyX/ng-virtual-list/blob/17.x/projects/ng-virtual-list/src/lib/utils/isCollectionMode.ts
+ * @author Evgenii Grebennikov
+ * @email djonnyx@gmail.com
+ */
+export const isCollectionMode = (src: CollectionMode, expected: CollectionMode): boolean => {
+    if (LAZY_ALIASES.includes(expected)) {
+        return LAZY_ALIASES.includes(src);
+    }
+    return NORMAL_ALIASES.includes(src);
+}
