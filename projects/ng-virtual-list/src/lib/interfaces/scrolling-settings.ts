6b5e0cc98eeb2c136f925ecad8481fcc6c850478
Evgenii Grebennikov <djonnyx@gmail.com>
2026-04-13 00:34:36 +0300

    feat: 20.x #767 scrolling settings have been implemented

---
+++ projects/ng-virtual-list/src/lib/interfaces/scrolling-settings.ts
@@ -0,0 +1,33 @@
+/**
+ * Interface IScrollingSettings.
+ * - frictionalForce - Frictional force. Default value is `0.035`.
+ * - mass - Mass. Default value is `0.005`.
+ * - maxDistance - Maximum scrolling distance. Default value is `12500`.
+ * - maxDuration - Maximum animation duration. Default value is `4000`.
+ * - speedScale - Speed scale. Default value is `15`.
+ * @link https://github.com/DjonnyX/ng-virtual-list/blob/20.x/projects/ng-virtual-list/src/lib/interfaces/scrolling-settings.ts
+ * @author Evgenii Alexandrovich Grebennikov
+ * @email djonnyx@gmail.com
+ */
+export interface IScrollingSettings {
+    /**
+     * Frictional force. Default value is `0.035`.
+     */
+    frictionalForce?: number;
+    /**
+     * Maximum animation duration. Default value is `4000`.
+     */
+    maxDuration?: number;
+    /**
+     * Mass. Default value is `0.005`.
+     */
+    mass?: number;
+    /**
+     * Maximum scrolling distance. Default value is `12500`.
+     */
+    maxDistance?: number;
+    /**
+     * Speed scale. Default value is `15`.
+     */
+    speedScale?: number;
+}
