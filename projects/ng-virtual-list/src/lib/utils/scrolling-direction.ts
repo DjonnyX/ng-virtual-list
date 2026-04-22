b924a3180f9d243d49030b9acf831e230b72d2c3
Evgenii Grebennikov <djonnyx@gmail.com>
2026-04-22 11:28:29 +0300

    refactor: 20x #783 optimized calculation of scroll direction property

--- projects/ng-virtual-list/src/lib/utils/scrolling-direction.ts
+++ projects/ng-virtual-list/src/lib/utils/scrolling-direction.ts
@@ -1,26 +1,20 @@
 type Dir = 1 | 0 | -1;
 
 export class ScrollingDirection {
-    private _values = new Array<Dir>();
+    private _value: number = 0;
 
     constructor(private _length: number = 5) { }
 
     add(v: Dir) {
-        this._values.push(v);
-        while (this._values.length > this._length) {
-            this._values.shift();
-        }
+        this._value += v;
     }
 
     get(): Dir {
-        let result: number = 0;
-        for (const v of this._values) {
-            result += v;
-        }
-        return result === 0 ? 0 : Math.sign(result) as Dir;
+        const v = this._value < this._length ? -this._length : this._value > this._length ? this._length : this._value;
+        return v === 0 ? 0 : Math.sign(v) as Dir;
     }
 
     clear() {
-        this._values = [];
+        this._value = 0;
     }
 }
