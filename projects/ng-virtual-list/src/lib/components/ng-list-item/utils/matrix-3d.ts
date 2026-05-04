bbd183f79e4f08481a531787acd3a5974a7346b1
Evgenii Grebennikov <djonnyx@gmail.com>
2026-05-04 07:51:04 +0300

    refactor: 20x #783 code review

--- projects/ng-virtual-list/src/lib/components/ng-list-item/utils/matrix-3d.ts
+++ projects/ng-virtual-list/src/lib/components/ng-list-item/utils/matrix-3d.ts
@@ -5,10 +5,14 @@
  * @email djonnyx@gmail.com
  */
 export const matrix3d = (x: number, y: number, z: number, scaleX: number, scaleY: number, scaleZ: number, rotationX: number, rotationY: number, rotationZ: number) => {
+  return rotationZ !== 0 ? `${__matrix3d__(x, y, z, scaleX, scaleY, scaleZ, rotationX, rotationY)} rotateZ(${rotationZ}deg)` : __matrix3d__(x, y, z, scaleX, scaleY, scaleZ, rotationX, rotationY);
+};
+
+const __matrix3d__ = (x: number, y: number, z: number, scaleX: number, scaleY: number, scaleZ: number, rotationX: number, rotationY: number) => {
   return `matrix3d(
           ${scaleX},        0,              ${rotationX},   0,
           0,                ${scaleY},      ${rotationY},   0,
           0,                0,              1,              0,
           ${x},             ${y},           ${z},           ${scaleZ}
-        ) rotateZ(${rotationZ}deg)`;
+        )`;
 };
