6c7265fe5a03b2e923b6e0edae4e6bbb854a2193
Evgenii Grebennikov <djonnyx@gmail.com>
2026-06-25 11:02:45 +0300

    hotfix: 16x #820 scrolling issues have been fixed

---
+++ projects/ng-virtual-list/src/lib/components/ng-scroller/utils/interfaces/calculate-scroll-metrics.ts
@@ -0,0 +1,11 @@
+/**
+ * ICalculateScrollMetrics
+ * @link https://github.com/DjonnyX/ng-virtual-list/blob/16.x/projects/ng-virtual-list/src/lib/components/scroller/utils/interfaces/calculate-scroll-metrics.ts
+ * @author Evgenii Alexandrovich Grebennikov
+ * @email djonnyx@gmail.com
+ */
+export interface ICalculateScrollMetrics {
+    gradientPos: [number, number];
+    size: number;
+    pos: number;
+}
