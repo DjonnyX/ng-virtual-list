0fa598b682d952ac25432833809e564fa60399be
Evgenii Grebennikov <djonnyx@gmail.com>
2026-07-18 20:16:30 +0300

    hotfix: 19x #860 fixed animation bugs when scrolling

--- projects/ng-virtual-list/src/lib/utils/event-emitter/event-emitter.ts
+++ projects/ng-virtual-list/src/lib/utils/event-emitter/event-emitter.ts
@@ -2,7 +2,7 @@ export type TEventHandler = (...args: Array<any>) => void;
 
 /**
  * Event emitter
- * @link https://github.com/DjonnyX/ng-virtual-list/blob/main/library/src/utils/event-emitter/event-emitter.ts
+ * @link https://github.com/DjonnyX/ng-virtual-list/blob/19.x/projects/ng-virtual-list/src/lib/utils/event-emitter/event-emitter.ts
  * @author Evgenii Alexandrovich Grebennikov
  * @email djonnyx@gmail.com
  */
